231632daac97cb75f354f514f6eefb542ffdd4d6
klendathu2k <jwebb@bnl.gov>
2022-10-21 20:31:42 -0400

    status code in shell does not reflect the failure of this macro...

--- unit_tests/unit_test_load_y2019a.C
+++ unit_tests/unit_test_load_y2019a.C
@@ -5,5 +5,6 @@ void unit_test_load_y2019a() {
   gROOT->LoadMacro("StRoot/macros/bfc.C");
   gROOT->ProcessLine("bfc(0,\"y2019a agml nodefault\");");
   gROOT->ProcessLine("chain->GetDataBase(\"VmcGeometry\");");
+  gROOT->ProcessLine("assert(gGeoManager);");
 }
 
