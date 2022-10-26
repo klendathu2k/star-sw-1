cb1a28cf262d2af2a71982fb73e0541253c62cfb
klendathu2k <jwebb@bnl.gov>
2022-10-25 23:06:54 -0400

    Recall... geometries are specified by timestamp.

--- unit_tests/unit_test_load_y2019a.C
+++ unit_tests/unit_test_load_y2019a.C
@@ -3,7 +3,7 @@
 void unit_test_load_y2019a() {
   std::cout << "unit_test_load_agml" << std::endl;
   gROOT->LoadMacro("StRoot/macros/bfc.C");
-  gROOT->ProcessLine("bfc(0,\"y2019a agml nodefault\");");
+  gROOT->ProcessLine("bfc(0,\"y2019a agml sdt20181220 nodefault\");");
   gROOT->ProcessLine("chain->GetDataBase(\"VmcGeometry\");");
   gROOT->ProcessLine("assert(gGeoManager);");
 }
