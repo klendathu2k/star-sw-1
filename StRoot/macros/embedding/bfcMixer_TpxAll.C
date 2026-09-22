86ad4402bcd35ae72dd004277094a19fe795562b
klendathu2k <jwebb@bnl.gov>
2026-09-22 17:28:52 -0400

    Fix typos in RNG seed initialization

--- StRoot/macros/embedding/bfcMixer_TpxAll.C
+++ StRoot/macros/embedding/bfcMixer_TpxAll.C
@@ -328,7 +328,7 @@ void bfcMixer_TpxAll()
       seed1=std::atoi( ss1.Data() );
     }
     if ( gSystem->Getenv("JOBINDEX") ) {
-      seed1 = std::atoi( gSystem->Getenv("JOB_INDEX") );
+      seed1 = std::atoi( gSystem->Getenv("JOBINDEX") );
     }
     else {
       auto ss2 = gSystem->GetFromPipe("od -An -N4 -tu4 /dev/urandom");
